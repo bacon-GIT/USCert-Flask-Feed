99ded6be8a5cd789ed7cdda70279d2f7e4cd3a1c
bacon-GIT <jbacon42@icloud.com>
2020-04-21 20:43:34 -0400

    Fixed the date glitch, cleaned things up. I also added a note about using the More_details variable in a useful way

--- mainflask.py
+++ mainflask.py
@@ -1,5 +1,6 @@
 # My example webpage as well as usage syntax.
 # I recommend you build something with a bit more flair
+# created by Josh Bacon github.com/bacon-GIT
 
 from flask import Flask, render_template
 import xmlparse
@@ -15,4 +16,5 @@ def table():
                             vuln3= xmlparse.parseAlerts(2),
                             vuln4= xmlparse.parseAlerts(3),
                             vuln5=xmlparse.parseAlerts(4),
-                            vuln6=xmlparse.parseAlerts(5))  # Expandable, just change the parameter
+                            vuln6=xmlparse.parseAlerts(5)) # Just using 5 as an example,
+                                                           # it can extend as far as the database can
--- xmlparse.py
+++ xmlparse.py
@@ -1,5 +1,5 @@
 import feedparser
-import time
+
 
 # parseAlerts grabs the (entry), extracts all necessary info, dumps the
 # rest into "more_details" and returns a list.
@@ -9,27 +9,25 @@ def parseAlerts(entry):
     feed = feedparser.parse("https://www.us-cert.gov/ncas/alerts.xml")
     # Sets $feed_entries to the current vulnerability
     feed_entries = feed.entries[entry]
-    # Extremely useful degugging line:
+
+    # useful degugging line:
     #print("Feed Entries: ", feed_entries)
 
     # Parse elements
     title = feed_entries['title']
     link = feed_entries['link']
-    summary_parsed = feed_entries['summary']
-    summary = summary_parsed[:72].strip("&")
+    summary = feed_entries['published']
 
-    # If entry has not been revised
-    if ("Summary") in summary:
-        summary = summary_parsed[:38]
-
-    # TODO
-    more_details = "click me"
+    more_details = feed_entries
 
     # Return items from list
     vuln_entry = []
-    vuln_entry.extend([title, link, summary, more_details])
+    vuln_entry.extend([title, link, summary])
     return vuln_entry
 
-#parseAlerts(1)
-#    title = entry[0]['title']
-#    description = entry[0]['title']
+
+'''
+NOTE ABOUT MORE_DETAILS
+I've left this pretty open ended, but my recommendation is to dump the
+rest of the variables from $feed_entries into it
+'''
